96941d2f23c2a9a9941ec0438689036ee5cc251a
ramaa25 <ahmadrama251005@gmail.com>
2026-01-02 00:02:59 +0700

    style: list stories

--- src/components/StoryContainer.vue
+++ src/components/StoryContainer.vue
@@ -9,6 +9,10 @@
     stories.getStories()
   });
   const openDialog = ref(false);
+
+  const showCreateDialog = () => {
+    openDialog.value = true;
+  };
 </script>
 
 <template>
@@ -16,13 +20,18 @@
     <div class="header">
       <h1>Stories 24 Hours</h1>
     </div>
-    <!-- <div
-      v-for="story in stories.story"
-      :key="story.id"
-      class="story-item"
-    >
-      <img :src="story.base64Image" alt="" style="max-width: 200px; max-height: 200px;" />
-    </div> -->
+    <div class="story-list">
+      <div class="create-story story-list-item" @click="showCreateDialog">
+        +
+      </div>
+      <div
+        v-for="story in stories.story"
+        :key="story.id"
+        class="story-list-item stories"
+      >
+        <img :src="story.base64Image" alt="">
+      </div>
+    </div>
   </div>
   <Dialog
     :show="openDialog"
@@ -31,12 +40,40 @@
   >
   <StoryForm />
   </Dialog>
-  <button @click="openDialog = true">Add Story</button>
 </template>
 
 <style>
   .header {
     text-align: center;
     padding: 1rem;
+  }
+  .story-list-item {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    cursor: pointer;
+    font-weight: bold;
+    overflow: hidden;
+  }
+  .create-story.story-list-item {
+    border: 3px solid #ccc;
+    color: #ccc
+  }
+  .stories.story-list-item {
+    border: 3px solid black;
+  }
+  .story-list {
+    display: flex;
+    gap: 1rem;
+    margin: 1rem;
+  }
+  .story-list-item img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 </style>
